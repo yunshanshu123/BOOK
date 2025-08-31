8206621582b191f5e6be622b3e2d50f7619e33a5
yunshanshu123 <3660343426@qq.com>
2025-09-01 00:38:38 +0800

    Create admin_insert.sql

---
+++ database/data/admin_insert.sql
@@ -0,0 +1,12 @@
+-- 以下数据均未插入数据库，仅作为代码参考
+
+-- /* ---------- 3. Librarian ---------- */
+
+-- INSERT ALL
+--   INTO Librarian (LibrarianID, Password, Name, Permission)
+--        VALUES ('LIB001', 'hash_pwd_001', '张伟', '高级')
+--   INTO Librarian (LibrarianID, Password, Name, Permission)
+--        VALUES ('LIB002', 'hash_pwd_002', '李娜', '高级')
+-- SELECT 1 FROM dual;
+
+-- -- 密码随便写的，请修改正确后提交到数据库
